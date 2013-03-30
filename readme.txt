b19295249bf0ddd53df063744a3798d776c882b0
Dominik Schilling <dominikschilling+git@gmail.com>
2013-03-30 11:43:55 +0100

    Added plugin files

---
+++ readme.txt
@@ -0,0 +1,104 @@
+=== Vine Embed ===
+Contributors: ocean90
+Tags: WP Embed, Embed, Video, Vine
+Donate link: https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=VR8YU922B7K46
+Requires at least: 2.9
+Tested up to: 3.6
+Stable tag: 0.1
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+Easily embed a Vine video into your posts.
+
+
+== Description ==
+
+Vine Embed is based on the new [API by Vine](https://vine.co/blog/embed-vine-posts).
+With Vine Embed you can easily share your little videos with other users.
+
+= What's your part? =
+Just place a Vine link into your posts. For example `https://vine.co/v/bjHh0zHdgZT`
+**By default no shortcodes are necessary, just a link!**
+And that was about your part. Easy, isn't it?
+
+= What does the plugin? =
+The plugin will fetch your links of your music and will convert it to a sexy Spotify Play Button.
+
+= Some usage examples =
+To embed a simple vide just embed a link like this:
+`https://vine.co/v/bjHh0zHdgZT`
+
+There are two different types of embeds, simple and postcard. You can see the difference [here](https://vine.co/v/bjHh0zHdgZT/embed).
+The default type is `simple`. To use the `postcard` type you had to choose the [embed] shortcode. Something like this:
+`[embed type="postcard"]https://vine.co/v/bjHh0zHdgZT[/embed]`
+
+For more examples please vist the [FAQ section](http://wordpress.org/extend/plugins/spotify-embed/faq/).
+
+**Sounds pretty good? Install now!**
+
+*Vine Embed is not associated in any way with Vine by Vine Labs, Inc..*
+
+= Feedback = 
+If you want, you can drop me a line @[ocean90](http://twitter.com/ocean90) on Twitter or @[Dominik Schilling](https://plus.google.com/101675293278434581718/) on Google+.
+
+= More =
+If you want, you can try also some of my [other plugins](http://profiles.wordpress.org/users/ocean90) or visit my site [wpGrafie.de](http://wpgrafie.de/).
+
+
+
+== Frequently Asked Questions ==
+
+= What about sizes? =
+Yes, there are three different sizes. Small, big, and huge. By default the size is based on [$content_width](http://codex.wordpress.org/Content_Width).
+Examples:
+`[embed size="small"]https://vine.co/v/bjHh0zHdgZT[/embed]`
+`[embed size="huge" type="postcard"]https://vine.co/v/bjHh0zHdgZT[/embed]`
+
+= Is it possible to change the default video type from `simple` to `postcard`? =
+Yes. You can use a filter:
+
+`
+function ds_vine_embed_default_type( $type ) {
+    return 'postcard',
+}
+add_filter( 'vine_embed_default_type', 'ds_vine_embed_default_type' );
+`
+
+= Can I combine all these keywords? =
+Yes. (But don't try to combine the size keywords, or the sky will fall on your head)
+
+
+
+== Screenshots ==
+
+1. An example of an embed video.
+
+
+
+== Installation ==
+
+Note: There will be NO settings page.
+
+For an automatic installation through WordPress:
+
+1. Go to the 'Add New' plugins screen in your WordPress admin area
+1. Search for 'Vine Embed'
+1. Click 'Install Now' and activate the plugin
+
+
+For a manual installation via FTP:
+
+1. Upload the `vine-embed` directory to the `/wp-content/plugins/` directory
+1. Activate the plugin through the 'Plugins' screen in your WordPress admin area
+
+
+To upload the plugin through WordPress, instead of FTP:
+
+1. Upload the downloaded zip file on the 'Add New' plugins screen (see the 'Upload' tab) in your WordPress admin area and activate.
+
+
+
+== Changelog ==
+
+= 0.1.0 =
+* First release.
---
+++ vine-embed.php
@@ -0,0 +1,117 @@
+<?php
+/**
+ * Plugin Name: Vine Embed
+ * Version: 0.1
+ * Description: Just add Vine links into your posts and the plugin will add the embed code for you.
+ * Author: Dominik Schilling
+ * Author URI: http://wphelper.de/
+ * Plugin URI: http://wpgrafie.de/wp-plugins/vine-embed/en/
+ *
+ * License: GPLv2 or later
+ *
+ *	Copyright (C) 2013 Dominik Schilling
+ *
+ *	This program is free software; you can redistribute it and/or
+ *	modify it under the terms of the GNU General Public License
+ *	as published by the Free Software Foundation; either version 2
+ *	of the License, or (at your option) any later version.
+ *
+ *	This program is distributed in the hope that it will be useful,
+ *	but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *	GNU General Public License for more details.
+ *
+ *	You should have received a copy of the GNU General Public License
+ *	along with this program; if not, write to the Free Software
+ *	Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+/**
+ * Don't call this file directly.
+ */
+if ( ! class_exists( 'WP' ) ) {
+	die();
+}
+
+
+/**
+ * The class.
+ */
+final class DS_Vine_Embed {
+	/**
+	 * The Vine API URL.
+	 */
+	private static $api_endpoint = 'https://vine.co/';
+
+	/**
+	 * Register the embed handler.
+	 */
+	public static function init() {
+		wp_embed_register_handler(
+			'vine',
+			'#https?:\/\/vine\.co\/v\/([a-z0-9]+)\/?#i',
+			array( __CLASS__, 'embed_handler_vine' )
+		);
+	}
+
+	/**
+	 * Callback function for the spotify embeds. Prints the HTML embed code.
+	 *
+	 * @param  mixed $matches Matches through the regex search.
+	 *               $matches[0] The Vine URL.
+	 *               $matches[1] The video ID.
+	 * @param  array $attr    Custom attributes from the [embed][/embed] shortcut
+	 * @param  string $url    The matched URL.
+	 * @param  array $rawattr Includes the height and width from media settings
+	 *
+	 * @return string         The HTML embed code.
+	 */
+	public static function embed_handler_vine( $matches, $attr, $url, $rawattr ) {
+		if ( empty( $matches ) )
+			return;
+
+		// The video ID
+		$id = $matches[1];
+
+		// Set the size of the embed iframe
+		if ( ! empty( $attr['size'] ) && 'small' == $attr['size'] ) {
+			$width = $height = 320;
+		} elseif ( ! empty( $attr['size'] ) && 'large' == $attr['size'] ) {
+			$width = $height = 480;
+		} elseif ( ! empty( $attr['size'] ) && 'huge' == $attr['size'] ) {
+				$width = $height = 600;
+		} else {
+			if ( ! empty( $rawattr['width'] ) && ! empty( $rawattr['height'] ) ) {
+				$width  = $rawattr['width'];
+				$height = $rawattr['height'];
+			} else {
+				list( $width, $height ) = wp_expand_dimensions( 320, 320, $attr['width'], $attr['height'] );
+			}
+		}
+
+		// Set the type, can be simple (default) or postcard
+		$type = apply_filters( 'vine_embed_default_type', 'simple' );
+		if ( ! empty( $attr['type'] ) && 'postcard' == $attr['type'] )
+			$type = 'postcard';
+
+		// Generate the base embed source
+		$embed_src = sprintf(
+			'%sv/%s/embed/%s',
+			self::$api_endpoint,
+			$id,
+			$type
+		);
+
+		// The embed code
+		return sprintf(
+			'<iframe class="vine-embed" src="%s" width="%d" height="%d" frameborder="0" allowTransparency="true"></iframe>%s',
+			esc_url( $embed_src ),
+			(int) $width,
+			(int) $height,
+			'<script async src="//platform.vine.co/static/scripts/embed.js" charset="utf-8"></script>'
+		);
+	}
+}
+
+// Gogogo.
+add_action( 'plugins_loaded', array( 'DS_Vine_Embed', 'init' ) );
